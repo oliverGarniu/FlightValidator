400bace90b69a97f161311cd981b3f82db9d10d5
oliverGarniu <oliver.garniu@gmail.com>
2023-05-29 18:22:11 +0200

    add test for FlightFeasibilityRule2

--- src/test/java/org/checkmyflight/service/FlightFeasibilityRule2Test.java
+++ src/test/java/org/checkmyflight/service/FlightFeasibilityRule2Test.java
@@ -75,15 +75,15 @@ class FlightFeasibilityRule2Test {
     }
 
     /*
-     * Testing Rule2 for flight between Paris and Los Angeles.
-     * Distance between PARIS and LOS ANGELES = 9104,34 km.
+     * Testing Rule2 for flight between Dublin and Barcelona.
+     * Distance between DUBLIN and BARCELONA = 1485 km.
      * takeOffTime = 21:44; lateTakeoffTime = 14:00; limitTakeOffTime = 20:00;
      * (takeOffTime > limitTakeOffTime) --> maxFlightRange = 0;
      *
      * expected result = false.
      */
     @Test
-    void evaluateRuleParisLosAngeles3() {
+    void evaluateRuleDublinBarcelona() {
 
         FlightService flightService = new FlightService();
         FlightDistance flightDistance = new FlightDistance(flightService);
@@ -93,12 +93,12 @@ class FlightFeasibilityRule2Test {
         flightService.localTime = LocalTime.of(21, 44);
         flightService.passengers = 202;
 
-        flightDistance.depLocation = "Paris";
-        flightDistance.latitude1 = 48.8566;
-        flightDistance.longitude1 = 2.3522;
-        flightDistance.arrLocation = "Los Angeles";
-        flightDistance.latitude2 = 33.941;
-        flightDistance.longitude2 = -118.4085;
+        flightDistance.depLocation = "Dublin";
+        flightDistance.latitude1 = 53.4264;
+        flightDistance.longitude1 = -6.2499;
+        flightDistance.arrLocation = "Barcelona";
+        flightDistance.latitude2 = 41.2974;
+        flightDistance.longitude2 = 2.0832;
 
         FlightFeasibilityRule2 feasibilityRule2 = new FlightFeasibilityRule2(flightService, flightFeasibilityRule1);
 
